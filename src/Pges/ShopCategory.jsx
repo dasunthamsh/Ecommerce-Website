041d7373f81cf37c7139bb3ecacd99d3a6fef7b6
Dasun Thamash <tunathamash@gmail.com>
2024-02-29 21:08:30 +0530

    ShopCategory.jsx completely done

--- src/Pges/ShopCategory.jsx
+++ src/Pges/ShopCategory.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, {useContext} from "react";
+import { ShopContext} from "../Context/ShopContext";
 
-const ShopCategory = () => {
+const ShopCategory = (props) => {
+    const {all_product} = useContext(ShopContext);
     return (
         <div>
-
+            <img src={props.banner}/>
         </div>
     )
 }
