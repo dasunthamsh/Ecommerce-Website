4259bb223661044acc4e7249957215db20a0710f
Dasun Thamash <tunathamash@gmail.com>
2024-03-01 01:22:14 +0530

    content updated

--- src/App.js
+++ src/App.js
@@ -20,9 +20,9 @@ function App() {
             <Navbar/>
             <Routes>
                 <Route path='/' element={<Shop/>}/>
-                <Route path='/mens' element={<ShopCategory banner={men_banner} category="mens" />}/>
-                <Route path='/womens' element={<ShopCategory banner={women_banner} category="wmens" />}/>
-                <Route path='/kids' element={<ShopCategory banner={kids_banner} category="kids" />}/>
+                <Route path='/mens' element={<ShopCategory banner={men_banner} category="men" />}/>
+                <Route path='/womens' element={<ShopCategory banner={women_banner} category="women" />}/>
+                <Route path='/kids' element={<ShopCategory banner={kids_banner} category="kid" />}/>
                 <Route path='/product' element={<Product/>}/>
                 <Route path=':productId' element={<Product/>}>
                 </Route>
