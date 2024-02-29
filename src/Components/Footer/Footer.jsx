1a2bda0877df6d99d3722cacd3c3e8f8d0250e22
Dasun Thamash <tunathamash@gmail.com>
2024-02-29 21:08:00 +0530

    Footer.jsx complete

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import footer_logo from '../../Assets/logo_big.png'
+import instagram_icon from '../../Assets/instagram_icon.png'
+import pintester_icon from '../../Assets/pintester_icon.png'
+import whatsapp_icon from '../../Assets/whatsapp_icon.png'
 
 const Footer = () => {
     return (
