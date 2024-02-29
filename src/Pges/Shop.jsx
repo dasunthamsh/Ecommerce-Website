460f3d8f9c34a4ccb8b3f084daa644e45c714372
Dasun Thamash <tunathamash@gmail.com>
2024-02-29 13:20:18 +0530

    Shop.js updated

--- src/Pges/Shop.jsx
+++ src/Pges/Shop.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Hero from "../Components/Hero/Hero";
 import Popular from "../Components/Popular/Popular";
+import Offers from "../Components/Offers/Offers";
+import New_collections from "../Assets/new_collections";
+import NewCollections from "../Components/NewCollections/NewCollections";
 
 const Shop = () => {
     return (
         <div>
             <Hero/>
             <Popular/>
+            <Offers/>
+            <NewCollections/>
         </div>
     )
 }
