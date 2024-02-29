41dac2a44df78cc32c8a6ec7be5e52872c1a3d51
Dasun Thamash <tunathamash@gmail.com>
2024-02-29 06:40:26 +0530

    header updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from "./Components/Navbar/Navbar";
-
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Shop from "./Pges/Shop";
+import ShopCategory from "./Pges/ShopCategory";
+import product from "./Pges/Product";
+import Product from "./Pges/Product";
+import LoginSignup from "./Pges/LoginSignup";
+import Cart from "./Pges/Cart";
 
 function App() {
   return (
     <div>
-      <Navbar/>
+        <BrowserRouter>
+            <Navbar/>
+            <Routes>
+                <Route path='/' element={<Shop/>}/>
+                <Route path='/mens' element={<ShopCategory category="mens" />}/>
+                <Route path='/womens' element={<ShopCategory category="wmens" />}/>
+                <Route path='/kids' element={<ShopCategory category="kids" />}/>
+                <Route path='/product' element={<Product/>}/>
+                <Route path=':productId' element={<Product/>}>
+                </Route>
+                <Route path='/cart' element={<Cart/>}/>
+                <Route path='/login' element={<LoginSignup/>}/>
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useState} from "react"
 import logo from "../../Assets/logo.png"
 import cart_icon from "../../Assets/cart_icon.png"
 import {Link} from "react-router-dom";
 
 const Navbar = () => {
+
+    const [menu,setMenu] = useState("Shop");
+
 
     return (
         <div className='navbar flex mt-30 mt-10 border-b pb-5'>
@@ -11,23 +14,15 @@ const Navbar = () => {
                 <img className='scale-75' src={logo} alt='logo'/>
                 <p className='font-bold text-3xl'>MODELER</p>
             </div>
-            {/*<ul className='nav-menu flex flex-row gap-10 mx-auto items-center font-bold'>*/}
-            {/*    <li onClick={()=>{setMenu("Shop")}}><Link to='/'>Shop</Link>{menu==="Shop" ? <hr style={{ backgroundColor: '#f00', height: '2px'}} />:null}</li>*/}
-            {/*    <li onClick={()=>{setMenu("Men")}}><Link to='/mens'>Men</Link>{menu==="Men" ?<hr style={{ backgroundColor: '#f00', height: '2px'}}/>:null}</li>*/}
-            {/*    <li onClick={()=>{setMenu("Women")}}><Link to='/womens'>Women</Link>{menu==="Women" ?<hr style={{ backgroundColor: '#f00', height: '2px'}}/>:null}</li>*/}
-            {/*    <li onClick={()=>{setMenu("Kids")}}><Link to='/kids'>Kids</Link>{menu==="Kids" ?<hr style={{ backgroundColor: '#f00', height: '2px'}}/>:null}</li>*/}
-            {/*</ul>*/}
             <ul className='nav-menu flex flex-row gap-10 mx-auto items-center font-bold'>
-                <li>Shop <hr style={{ backgroundColor: '#f00', height: '2px'}}/></li>
-                <li>Men <hr style={{ backgroundColor: '#f00', height: '2px'}}/></li>
-                <li>Women <hr style={{ backgroundColor: '#f00', height: '2px'}}/></li>
-                <li>Kids <hr style={{ backgroundColor: '#f00', height: '2px'}}/></li>
+                <li onClick={()=>setMenu("shop")}><Link to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
+                <li onClick={()=>setMenu("Men")}><Link to='/mens'>Men</Link>{menu==="Men"?<hr/>:<></>}</li>
+                <li onClick={()=>setMenu("Women")}><Link to='/womens'>Women</Link>{menu==="Women"?<hr/>:<></>}</li>
+                <li onClick={()=>setMenu("Kids")}><Link to='/kids'>Kids</Link>{menu==="Kids"?<hr/>:<></>}</li>
             </ul>
             <div className='nav-login-cart flex mr-32 gap-5'>
-                {/*<Link to='/login'><button >Login</button></Link>*/}
-                {/*<Link to='/cart'><img className='scale-50' src={cart_icon} alt='cart'/></Link>*/}
-                <button>Login</button>
-                <img className='scale-50' src={cart_icon} alt=""/>
+                <Link to='/login'><button >Login</button></Link>
+                <Link to='/cart'><img className='scale-50' src={cart_icon} alt='cart'/></Link>
                 <div><div className='relative right-8 bg-red-600 bg-auto rounded-full'>0</div></div>
             </div>
         </div>
