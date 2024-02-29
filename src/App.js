976d66584b87e54eb83732f4ad2202501f002b03
Dasun Thamash <tunathamash@gmail.com>
2024-02-29 21:07:07 +0530

    App.jsx Routeing updated

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import product from "./Pges/Product";
 import Product from "./Pges/Product";
 import LoginSignup from "./Pges/LoginSignup";
 import Cart from "./Pges/Cart";
+import Footer from "./Components/Footer/Footer";
+import men_banner from "./Assets/banner_mens.png"
+import kids_banner from "./Assets/banner_kids.png"
+import women_banner from "./Assets/banner_women.png"
 
 function App() {
   return (
@@ -16,15 +20,16 @@ function App() {
             <Navbar/>
             <Routes>
                 <Route path='/' element={<Shop/>}/>
-                <Route path='/mens' element={<ShopCategory category="mens" />}/>
-                <Route path='/womens' element={<ShopCategory category="wmens" />}/>
-                <Route path='/kids' element={<ShopCategory category="kids" />}/>
+                <Route path='/mens' element={<ShopCategory banner={men_banner} category="mens" />}/>
+                <Route path='/womens' element={<ShopCategory banner={women_banner} category="wmens" />}/>
+                <Route path='/kids' element={<ShopCategory banner={kids_banner} category="kids" />}/>
                 <Route path='/product' element={<Product/>}/>
                 <Route path=':productId' element={<Product/>}>
                 </Route>
                 <Route path='/cart' element={<Cart/>}/>
                 <Route path='/login' element={<LoginSignup/>}/>
             </Routes>
+            <Footer/>
         </BrowserRouter>
     </div>
   );
