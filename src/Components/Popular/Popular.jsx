e7b44de0087ad0d95610c0e64a8e897e4e069f92
Dasun Thamash <tunathamash@gmail.com>
2024-02-29 12:43:00 +0530

    popular.js added

---
+++ src/Components/Popular/Popular.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import data_product from "../../Assets/data"
+import Item from "../Item/Item";
+
+const Popular = ()=>{
+    return (
+        <div>
+            <div className='mt-20'>
+                <h1 className='flex justify-center underline text-4xl font-bold'>POPLAR IN WOMEN</h1>
+                <div className='flex gap-7 m-28'>
+                    {/*loop everything in item class in here*/}
+                    {data_product.map((item,i)=>{
+                        return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+                    })}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Popular;
