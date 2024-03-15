4f1240ea22b4247ec0c1f0f68da96643e29a3724
Dasun Thamash <tunathamash@gmail.com>
2024-03-15 12:20:54 +0530

    add new routes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Navbar from "./Components/Navbar/Navbar";
 import {BrowserRouter,Routes,Route} from "react-router-dom";
-import Shop from "./Pges/Shop";
-import ShopCategory from "./Pges/ShopCategory";
-import product from "./Pges/Product";
-import Product from "./Pges/Product";
-import LoginSignup from "./Pges/LoginSignup";
-import Cart from "./Pges/Cart";
+import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
-import men_banner from "./Assets/banner_mens.png"
-import kids_banner from "./Assets/banner_kids.png"
-import women_banner from "./Assets/banner_women.png"
+import Home from "./Pages/Home";
+import ForestAdventure from "./Pages/Adventure/ForestAdventure";
 
 function App() {
   return (
     <div>
         <BrowserRouter>
-            <Navbar/>
+            <Header/>
             <Routes>
-                <Route path='/' element={<Shop/>}/>
-                <Route path='/mens' element={<ShopCategory banner={men_banner} category="men" />}/>
-                <Route path='/womens' element={<ShopCategory banner={women_banner} category="women" />}/>
-                <Route path='/kids' element={<ShopCategory banner={kids_banner} category="kid" />}/>
-                <Route path='/product' element={<Product/>}/>
-                <Route path=':productId' element={<Product/>}>
-                </Route>
-                <Route path='/cart' element={<Cart/>}/>
-                <Route path='/login' element={<LoginSignup/>}/>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/forestadventure' element={<ForestAdventure/>}/>
+
+
             </Routes>
             <Footer/>
         </BrowserRouter>
