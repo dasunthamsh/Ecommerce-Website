3fd1250082038fc7a924d6df0b0ca5f932b14307
Dasun Thamash <tunathamash@gmail.com>
2024-03-16 01:25:02 +0530

    Culture section added

---
+++ src/Components/Culture/Culture.jsx
@@ -0,0 +1,129 @@
+import React from "react";
+import divImg from "../../Assets/nelum.png"
+import imgOne from "../../Assets/hotelOne.jpg";
+
+const Culture = () => {
+    return (
+
+        <>
+
+            <div className=" dark:bg-zinc-900">
+
+                <h1 className="text-5xl font-bold flex justify-center p-20  ">Top Things To Do In Sri Lanka</h1>
+
+                <div className="flex flex-wrap justify-center">
+                    {/* First Row */}
+                    <div>
+                        <div>
+                            <div className="max-w-80 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-2">
+                                <a href="#">
+                                    <img className="rounded-t-lg" src={imgOne} alt="" />
+                                </a>
+                                <div className="p-5">
+                                    <a href="#">
+                                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Sigiriya The Ancient Rock Fortress</h5>
+                                    </a>
+                                    <h6 className="pt-3 pb-3">Ancient Ruins</h6>
+                                    <h6>Sigiriya, also known as Lion Rock, is an ancient rock fortress located in the central Matale District of Sri Lanka. It is one of the island nation's most iconic landmarks and is a UNESCO World Heritage Site. Here are some key points about Sigiriya:</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div>
+                            <div className="max-w-80 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-2">
+                                <a href="#">
+                                    <img className="rounded-t-lg" src={imgOne} alt="" />
+                                </a>
+                                <div className="p-5">
+                                    <a href="#">
+                                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Temple of the Sacred Tooth Relic</h5>
+                                    </a>
+                                    <h6 className="pt-3 pb-3">Ancient Ruins</h6>
+                                    <h6>
+                                        The Temple of the Sacred Tooth Relic, also known as Sri Dalada Maligawa, is a Buddhist temple located in the city of Kandy, Sri Lanka. It is one of the most sacred places of worship for Buddhists around the world, as it houses the relic of the tooth of the Buddha.</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div>
+                            <div className="max-w-80 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-2">
+                                <a href="#">
+                                    <img className="rounded-t-lg" src={imgOne} alt="" />
+                                </a>
+                                <div className="p-5">
+                                    <a href="#">
+                                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                                    </a>
+                                    <h6 className="pt-3 pb-3">Ancient Ruins</h6>
+                                    <h6>The ‘Rock’ is a monolith that rises 200 vertical metres from the surrounding plain, which was then, in an extraordinary…</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="flex flex-wrap justify-center mt-5">
+                    {/* second Row */}
+                    <div>
+                        <div>
+                            <div className="max-w-80 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-2">
+                                <a href="#">
+                                    <img className="rounded-t-lg" src={imgOne} alt="" />
+                                </a>
+                                <div className="p-5">
+                                    <a href="#">
+                                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                                    </a>
+                                    <h6 className="pt-3 pb-3">Ancient Ruins</h6>
+                                    <h6>The ‘Rock’ is a monolith that rises 200 vertical metres from the surrounding plain, which was then, in an extraordinary…</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div>
+                            <div className="max-w-80 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-2">
+                                <a href="#">
+                                    <img className="rounded-t-lg" src={imgOne} alt="" />
+                                </a>
+                                <div className="p-5">
+                                    <a href="#">
+                                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                                    </a>
+                                    <h6 className="pt-3 pb-3">Ancient Ruins</h6>
+                                    <h6>The ‘Rock’ is a monolith that rises 200 vertical metres from the surrounding plain, which was then, in an extraordinary…</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div>
+                            <div className="max-w-80 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-2">
+                                <a href="#">
+                                    <img className="rounded-t-lg" src={imgOne} alt="" />
+                                </a>
+                                <div className="p-5">
+                                    <a href="#">
+                                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                                    </a>
+                                    <h6 className="pt-3 pb-3">Ancient Ruins</h6>
+                                    <h6>The ‘Rock’ is a monolith that rises 200 vertical metres from the surrounding plain, which was then, in an extraordinary…</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default Culture;
