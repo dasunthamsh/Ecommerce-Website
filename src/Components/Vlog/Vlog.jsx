9755c7a27009091dd105382df606f371980cc6e4
Dasun Thamash <tunathamash@gmail.com>
2024-03-16 01:26:25 +0530

    vlog page added

---
+++ src/Components/Vlog/Vlog.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import imgOne from "../../Assets/hotelOne.jpg";
+import imgTwo from "../../Assets/hotelTwo.jpg";
+import imgThree from "../../Assets/hotelThree.jpg";
+import imgFour from "../../Assets/hotelFour.jpg";
+
+const Vlog = () => {
+
+    return(
+
+        <>
+
+            <div className="dark:bg-zinc-900 pb-20">
+
+                <h1 className="text-5xl font-bold flex justify-center p-20 ">Vlog</h1>
+
+                <div className="flex justify-center gap-5">
+
+                    <div>
+                        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                            <a href="#">
+                                <img className="rounded-t-lg" src={imgOne} alt="" />
+                            </a>
+                            <div className="p-5">
+                                <a href="#">
+                                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                            <a href="#">
+                                <img className="rounded-t-lg" src={imgTwo} alt="" />
+                            </a>
+                            <div className="p-5">
+                                <a href="#">
+                                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                            <a href="#">
+                                <img className="rounded-t-lg" src={imgThree} alt="" />
+                            </a>
+                            <div className="p-5">
+                                <a href="#">
+                                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                            <a href="#">
+                                <img className="rounded-t-lg" src={imgFour} alt="" />
+                            </a>
+                            <div className="p-5">
+                                <a href="#">
+                                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </>
+    )
+}
+export default Vlog;
