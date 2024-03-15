3ac425919a51283b0bbef26751eaaebea20af62f
Dasun Thamash <tunathamash@gmail.com>
2024-03-16 01:25:42 +0530

    Festival function added

---
+++ src/Components/Festivals/Festivals.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import imgSri from "../../Assets/elephant.jpg";
+
+const Festivals = () => {
+
+    return (
+
+        <>
+            <div className="dark:bg-zinc-900">
+                <div className="p-20 rounded-3xl h-4/5">
+                    <img className=" rounded-3xl w-full h-full object-cover" src={imgSri}/>
+                </div>
+            </div>
+
+
+        </>
+        )
+}
+
+export default Festivals;
