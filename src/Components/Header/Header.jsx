7bcb66121288364b6594bea991e3d75b75775726
Dasun Thamash <tunathamash@gmail.com>
2024-03-18 12:01:46 +0530

    update logo to Header.jsx

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import logo from "../../Assets/logo.png";
 
 const Header = (props) => {
     return (
@@ -8,8 +9,8 @@ const Header = (props) => {
                 <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
                     <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                         <a href="https://flowbite.com" className="flex items-center">
-                            <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-                            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                            <img src={logo} className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+                            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Taprobane</span>
                         </a>
                         <div className="flex items-center lg:order-2">
                             <a href="#" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log in</a>
